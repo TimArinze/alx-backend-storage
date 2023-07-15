e68701a83894775eb255f3ad353840f4811f7430
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-15 16:18:12 +0100

    task

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,4 @@
+-- script that creates an index idx_name_first on the table names 
+-- and the first letter of name
+ALTER TABLE names ADD first_letter CHAR(1) AS (LEFT(name, 1));
+CREATE INDEX idx_name_first ON names (first_letter);
