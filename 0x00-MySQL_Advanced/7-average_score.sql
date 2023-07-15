de0e3f90e985d53cf1d4506aaefa62ebe2690c8d
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-15 01:47:01 +0100

    task

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,7 +5,9 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	UPDATE users
-	SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
+	SET average_score = (
+	SELECT AVG(score) FROM corrections WHERE user_id = corrections.user_id
+	)
 	WHERE id = user_id;
 END
 $$
