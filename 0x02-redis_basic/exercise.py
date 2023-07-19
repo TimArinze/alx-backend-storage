3f6a1f4e96ae1038eef65e778856280f86708796
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-20 00:54:36 +0100

    task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Writing strings to Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -19,3 +19,21 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Callable =None):
+        """Reading from Redis and recovering original type"""
+        if key is None:
+            return None
+        if fn:
+            return fn(self._redis.get(key))
+        else:
+            return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """Reading from Redis and recovering original type"""
+        return str(self.get(key))
+    
+    def get_int(self, key: str) -> int:
+        """Reading from Redis and recovering original type"""
+        return int(self.get(key))
+    
