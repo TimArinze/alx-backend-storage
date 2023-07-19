03a910052aa66a7a021c0f0243aac7227d570707
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-20 00:13:28 +0100

    task

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Writing strings to Redis
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """Writing strings to Redis"""
+    def __init__(self) -> None:
+        """Constructor method"""
+        self._redis = redis.Redis()
+        self.flush = self._redis.flushdb()
+    def store(self, data: Union[float, str, int, bytes]) -> str:
+        """Method that takes a data argument and returns a string"""
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return random_key
