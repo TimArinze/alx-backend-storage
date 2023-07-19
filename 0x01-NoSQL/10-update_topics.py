86256e6269b197785b8ed9e54b4acc3c3f8b8617
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-19 02:03:26 +0100

    task10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -9,5 +9,5 @@ def update_topics(mongo_collection, name, topics):
     function that changes all topics of a school document based on the name
     """
     myquery = {"name" : name}
-    newvalues = {"$set": {"name": topics}}
-    mongo_collection.update_one(myquery, newvalues)
+    newvalues = {"$set": {"topics": topics}}
+    mongo_collection.update_many(myquery, newvalues)
