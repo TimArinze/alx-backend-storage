6da9c4ff9b77baca031f80110e1d81d7b3162067
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-19 01:59:26 +0100

    task10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Change school topics
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    function that changes all topics of a school document based on the name
+    """
+    myquery = {"name" : name}
+    newvalues = {"$set": {"name": topics}}
+    mongo_collection.update_one(myquery, newvalues)
