a053736f104e06f8c10871b04f5c33b6c4a0f9a6
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-20 00:19:21 +0100

    task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,11 +9,11 @@ from typing import Union
 
 class Cache:
     """Writing strings to Redis"""
-    def __init__(self: 'Cache') -> None:
+    def __init__(self) -> None:
         """Constructor method"""
         self._redis = redis.Redis()
-        self.flush = self._redis.flushdb()
-    def store(self: 'Cache', data: Union[float, str, int, bytes]) -> str:
+        self._redis.flushdb()
+    def store(self, data: Union[float, str, int, bytes]) -> str:
         """Method that takes a data argument and returns a string"""
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
