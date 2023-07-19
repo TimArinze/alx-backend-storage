0e49520245e0b86dc0211f33557b0927ff1dc64b
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-19 02:14:36 +0100

    task11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,4 +6,4 @@ Where can I learn Python
 
 def schools_by_topic(mongo_collection, topic):
     """function that returns the list of school having a specific topic"""
-    return mongo_collection.find({"topic": topic})
+    return mongo_collection.find({"topics": topic})
